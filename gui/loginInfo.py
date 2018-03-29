b542cbeed8dbfe8072683ee4bb09ec9e4634b74b
whitedann <MNP-SDXBENCH-01@idtdna.com>
2018-03-29 14:45:18 -0700

    new 3/29 changes

---
+++ gui/loginInfo.py
@@ -0,0 +1,15 @@
+class loginInfo:
+
+    def __init__(self,username,password):
+        self.user = username
+        self.password = password
+
+    def getUsername(self):
+        return self.user
+
+    def getPassword(self):
+        return self.password
+
+    
+
+    
