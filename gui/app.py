33bd544351cd96cfe1f3b175fa64eb984590f698
whitedann <MNP-SDXBENCH-01@idtdna.com>
2018-03-29 14:44:02 -0700

    3/29 work changes

--- gui/app.py
+++ gui/app.py
@@ -1,5 +1,4 @@
 import sys
-import p2.py
 from PyQt5 import QtWidgets
 
 
@@ -42,9 +41,68 @@ class Window(QtWidgets.QWidget):
     def btn_clk(self):
         sender = self.sender()
         if sender.text() == 'Generate':
-            print('success')
+            login_Attempt = loginWindow()
+            #login, and script here
             self.customerLine.clear()
             self.barcodeLine.clear()
+
+class loginWindow(QtWidgets.QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.init_window()
+
+    def init_window(self):
+        self.loginButton = QtWidgets.QPushButton('Login')
+        self.usernameLine = QtWidgets.QLineEdit(maxLength=10)
+        self.passwordLine = QtWidgets.QLineEdit()
+        self.usernameLabel = QtWidgets.QLabel('Username')
+        self.passwordLabel = QtWidgets.QLabel('Password')
+
+        password_box = QtWidgets.QHBoxLayout()
+        password_box.addWidget(self.passwordLabel)
+        password_box.addWidget(self.passwordLine)
+
+        user_box = QtWidgets.QHBoxLayout()
+        user_box.addWidget(self.usernameLabel)
+        user_box.addWidget(self.usernameLine)
+
+        bottom_box = QtWidgets.QHBoxLayout()
+        bottom_box.addWidget(self.loginButton)
+
+        v_box = QtWidgets.QVBoxLayout()
+        v_box.addLayout(user_box)
+        v_box.addLayout(password_box)
+        v_box.addLayout(bottom_box)
+
+        self.setLayout(v_box)
+        self.setWindowTitle('Login')
+
+        self.generateButton.clicked.connect(self.btn_clk)
+
+    
+
+    def btn_clk(self):
+
+        sender = self.sender()
+        if sender.text() == 'Login':
+            self.username = self.usernameLine.text()
+            self.password = self.passwordLine.text()
+
+    def get_User(self):
+
+        return self.username;
+
+    def get_Password(self):
+
+        return self.password;
+
+
+                   
+
+
+
+        
         
             
 
